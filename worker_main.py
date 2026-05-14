39c6304ba8c199a9b4019e1f61d2a12c919b10a5
se1dhe <91713064+se1dhe@users.noreply.github.com>
2026-05-14 15:41:18 +0300

    Add simple worker entrypoint

--- worker_main.py
+++ worker_main.py
@@ -1,1 +1,8 @@
-print('worker')
+import os
+from redis import Redis
+from rq import SimpleWorker
+
+REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+connection = Redis.from_url(REDIS_URL)
+worker = SimpleWorker(['render'], connection=connection)
+worker.work()
