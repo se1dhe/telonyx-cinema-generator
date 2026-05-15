77506e7cfee128f20428458da7588f686efa3d7f
se1dhe <91713064+se1dhe@users.noreply.github.com>
2026-05-15 09:08:59 +0300

    Remove legacy root worker entrypoint

--- worker_main.py
+++
@@ -1,8 +0,0 @@
-import os
-from redis import Redis
-from rq import SimpleWorker
-
-REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
-connection = Redis.from_url(REDIS_URL)
-worker = SimpleWorker(['render'], connection=connection)
-worker.work()
